3e9dae0d5997f47de1f0cdd3bd75b16bf604546b
kimcitaw <kimcitaw@gmail.com>
2019-08-25 19:54:08 -0300

    Todos los de objetos terminados

---
+++ Ejercicios DOM/Ejercicios DOM Eze/ejercicioDOM02.js
@@ -0,0 +1,7 @@
+var importante = document.getElementById('importante');
+var item = document.querySelectorAll('li');
+var lista = document.querySelector('ul');
+
+console.log(importante);
+console.log(item);
+console.log(lista);
